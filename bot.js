5b65c30504e95640c37ee3d1e4162ffa7dcdaa07
elitegang <44949165+elitegang@users.noreply.github.com>
2018-11-13 05:39:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "508734532174872587"; // ايدي السررفر
+var channel = "508978339499737088";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad , Sad ,Sad , Sad , Sad , Sad , Sad , Sad ,Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad ,Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad , Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad ,Sad , **')
     },305);
 })
 
